58b418d760f4c9261ddb7c4f797e4669f505ccc4
Bichu-B-Binu <bichubbinu77@gmail.com>
2025-09-22 12:18:12 +0530

    reverse

---
+++ script.js
@@ -0,0 +1,15 @@
+// console.log("Hello World");
+// for (let i = 0; i <= 5; i++) {
+//   console.log("Numbers:", i);
+// }
+
+// const twoSum = (nums, target) => {
+//   nums = [2, 7, 11, 15];
+//   target = 9;
+
+//   for (let i = 0; i < nums.length; i++) {
+    
+//   }
+// };
+
+// twoSum();
