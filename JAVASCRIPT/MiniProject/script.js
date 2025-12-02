d0eb0925316e723438abd18713613bd9914e995f
Bichu-B-Binu <bichubbinu77@gmail.com>
2025-12-02 14:00:17 +0530

    Mini Project

--- JAVASCRIPT/MiniProject/script.js
+++ JAVASCRIPT/MiniProject/script.js
@@ -3,7 +3,7 @@ const courses = [
   {
     name: "javaScript",
     price: 1000,
-    rating: 4.57,
+    ratting: 4.57,
     isAvailable: false,
     languages: ["Malayalam", "Tamil"],
     mentor: { name: "subin", contact: "99-223-34344" },
@@ -11,7 +11,7 @@ const courses = [
   {
     name: "cyber Security",
     price: 2000,
-    rating: 5.87,
+    ratting: 5.87,
     isAvailable: false,
     languages: ["Malayalam", "Tamil"],
     mentor: { name: "rahul", contact: "99-223-34344" },
@@ -19,7 +19,7 @@ const courses = [
   {
     name: "flutter",
     price: 3000,
-    rating: 7.27,
+    ratting: 7.27,
     isAvailable: false,
     languages: ["Malayalam", "Tamil"],
     mentor: { name: "deepak", contact: "99-223-34344" },
@@ -27,7 +27,7 @@ const courses = [
   {
     name: "django",
     price: 4000,
-    rating: 9.47,
+    ratting: 9.47,
     isAvailable: false,
     languages: ["Malayalam", "Tamil"],
     mentor: { name: "manoj", contact: "99-223-34344" },
@@ -35,7 +35,7 @@ const courses = [
   {
     name: "data Science",
     price: 5000,
-    rating: 7.57,
+    ratting: 7.57,
     isAvailable: false,
     languages: ["Malayalam", "Tamil"],
     mentor: { name: "vishnu", contact: "99-223-34344" },
@@ -43,7 +43,7 @@ const courses = [
   {
     name: "mechine Learning",
     price: 6000,
-    rating: 8.87,
+    ratting: 8.87,
     isAvailable: false,
     languages: ["Malayalam", "Tamil"],
     mentor: { name: "renu", contact: "99-223-34344" },
@@ -53,14 +53,21 @@ const courses = [
 // Function to format and display courses
 const formatName = (name) => name.charAt(0).toUpperCase() + name.slice(1);
 
+// Display total course count
+const displayTotalCourseCount = (courses) => {
+  const courseCount = document.querySelector("#course-count");
+  courseCount.innerText = `Total Courses: ${courses.length}`;
+};
+
 // Display total number of courses
 function displayCourses(courses) {
   const courseContainer = document.querySelector("#course-container");
+    courseContainer.innerHTML = "";
   courses.forEach((course) => {
     const courseUI = `<div class="course-card">
             <h3>${formatName(course.name)}</h3>
             <p>Price: ${course.price}</p>
-            <p>Rating: ${Math.round(course.rating * 10) / 10}</p>
+            <p>Rating: ${Math.round(course.ratting * 10) / 10}</p>
             <p>Available Languages: ${course.languages}</p>
             <p>Instructor: ${formatName(course.mentor.name)}</p>
             <p>Contact: ${course.mentor.contact}</p>
@@ -68,9 +75,56 @@ function displayCourses(courses) {
     courseContainer.innerHTML = courseContainer.innerHTML + courseUI;
   });
 }
-displayCourses(courses);
 
-// Display total course count
-const courseCount = document.querySelector("#course-count");
+// Find top rated course
+const findtopRatedCourse = (courses) => {
+  const topRated = courses.reduce((acc, course) => {
+    return acc.ratting > course.ratting ? acc : course;
+  });
+  return topRated;
+};
 
-courseCount.innerText = `Total Courses: ${courses.length}`;
+// Display top rated course
+displyaTopRatedCourses = (coursesArr) => {
+  const topRatedCourse = findtopRatedCourse(coursesArr);
+  const top = document.querySelector("#top-rated-course");
+
+  const courseUI = `<div class="course-card">
+                        <h3>${formatName(topRatedCourse.name)}</h3>
+                        <p>Price: ${topRatedCourse.price}</p>
+                        <p>Rating: ${
+                          Math.round(topRatedCourse.ratting * 10) / 10
+                        }</p>
+                        <p>Available Languages: ${topRatedCourse.languages}</p>
+                        <p>Instructor: ${formatName(
+                          topRatedCourse.mentor.name
+                        )}</p>
+                        <p>Contact: ${topRatedCourse.mentor.contact}</p>
+                    </div>`;
+
+  top.innerHTML = courseUI;
+};
+
+// Search functionality
+
+const getSearchesult = (query, courses) => {
+  const result = courses.filter((course) => {
+    return course.name.includes(query);
+  });
+  displayCourses(result);
+  displayTotalCourseCount(result);
+  
+};
+
+
+// Initialize page
+const initPage = () => {
+  displayTotalCourseCount(courses);
+  displayCourses(courses);
+  displyaTopRatedCourses(courses);
+  const searchInput = document.querySelector("#search");
+  searchInput.addEventListener("input", (e) => {
+    getSearchesult(e.target.value, courses);
+  });
+};
+initPage();
